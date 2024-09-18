37580856f61e7fd902764302a68ead6547b2a463
katya-sy <katya_sy_28@mail.ru>
2024-09-18 14:58:23 +0900

    project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,37 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xs: "480px",
+      sm: "576px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+      "2xl": "1536px",
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        xs: "100%",
+        sm: "100%",
+        md: "100%",
+        lg: "100%",
+        xl: "1150px",
+        "2xl": "1150px",
+      },
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: {
+          DEFAULT: "#415167",
+          dark: "#151d28",
+        },
+        beige: {
+          DEFAULT: "#c7a17a",
+          light: "#f9f5e8",
+        },
+        grey: "#edf0f5",
       },
     },
   },
