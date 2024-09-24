824be61fe18df940477edfb00e604d8abe160d11
katya-sy <katya_sy_28@mail.ru>
2024-09-24 21:00:56 +0900

    adaptive for mobile

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,21 @@
+'use client'
 import Image from 'next/image'
 import { Cart } from './shared/cart'
+import { useState } from 'react'
 
 export const Header = () => {
+  const [open, setOpen] = useState(false)
+
   return (
     <div className="bg-beige-light">
-      <div className="relative after:top-0 after:bottom-0 after:left-[calc(100%-16px)] after:absolute flex items-stretch max-md:after:hidden after:bg-blue max-md:pr-0 after:w-[calc((100vw-100%)/2+20px)] container">
-        <div className="relative flex flex-col justify-between mt-16 max-md:mt-10">
+      <div className="md:relative after:top-0 after:bottom-0 after:left-[calc(100%-16px)] after:absolute flex items-stretch max-md:after:hidden after:bg-blue max-md:pr-0 max-sm:pl-0 after:w-[calc((100vw-100%)/2+20px)] container">
+        <div
+          onClick={() => setOpen(false)}
+          className={`${
+            open ? 'absolute md:hidden inset-0 z-[3] bg-black/60' : 'hidden'
+          } md:hidden`}
+        />
+        <div className="relative max-sm:top-10 max-sm:absolute max-sm:inset-4 flex flex-col justify-between max-sm:gap-5 mt-16 max-sm:mt-0 max-md:mt-10">
           <Image
             src="/logo.svg"
             width={110}
@@ -13,27 +23,41 @@ export const Header = () => {
             alt="Logo"
             className="min-w-max"
           />
-          <p className="bottom-0 absolute flex flex-col mb-16 max-md:mb-10 font-black font-montserrat text-[54px] text-blue max-md:text-5xl uppercase max-md:leading-tight">
+          <p className="bottom-0 absolute flex flex-col mb-16 max-sm:mb-0 max-md:mb-10 font-black font-montserrat text-[54px] text-grey sm:text-blue max-sm:text-4xl max-md:text-5xl max-[330px]:text-3xl uppercase max-md:leading-tight">
             <span>Your</span>
-            <span className="relative after:top-0 after:-right-4 after:bottom-0 after:-left-4 z-10 after:-z-10 after:absolute flex justify-center items-center after:bg-beige text-white">
+            <span className="relative after:top-0 max-sm:after:-right-2 after:-right-4 after:bottom-0 max-sm:after:-left-2 after:-left-4 z-[2] after:-z-[2] after:absolute flex justify-center items-center after:bg-beige max-sm:w-max text-grey">
               personalized
             </span>
             <span>coffee</span>
           </p>
         </div>
-        <div className="flex flex-shrink ml-auto max-md:w-3/5 max-[880px]:max-w-sm max-[1100px]:max-w-lg">
+        <div className="flex flex-shrink sm:ml-auto max-sm:w-screen max-sm:max-w-[575px] max-[880px]:max-w-sm max-[1100px]:max-w-lg max-sm:h-screen">
           <Image
-            className="max-w-full object-cover"
+            className="w-full object-cover"
             src="/header-image.png"
             width={688}
             height={768}
             alt="Image"
           />
         </div>
-        <div className="flex flex-col flex-shrink-0 gap-28 max-md:hidden bg-blue py-16 pr-6 pl-14 max-lg:pl-8">
-          <button className="relative w-max">
-            <Cart />
-            <div className="-top-3 -right-3 absolute flex justify-center items-center bg-beige rounded-full w-5 h-5 text-grey text-sm">
+        <button
+          onClick={() => setOpen(true)}
+          className="top-10 right-2 absolute md:hidden text-grey hover:text-beige transition-colors"
+        >
+          <Cart />
+        </button>
+        <div
+          className={`${
+            open
+              ? 'max-md:right-0 max-md:flex'
+              : 'max-md:-right-full max-md:hidden'
+          } max-md:absolute flex flex-col flex-shrink-0 gap-28 bg-blue py-16 pr-6 pl-14 max-lg:pl-8 max-md:px-14 max-md:gap-16 max-md:py-10 max-md:h-full max-md:z-[3]`}
+        >
+          <button className="relative w-max text-grey">
+            <div className="hover:text-beige transition-colors">
+              <Cart />
+            </div>
+            <div className="-top-3 -right-3 absolute flex justify-center items-center bg-beige rounded-full w-5 h-5 text-sm">
               2
             </div>
           </button>
