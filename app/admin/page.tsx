f214e1087618a09b0ce59047dac9142aa3818c2c
katya-sy <katya_sy_28@mail.ru>
2024-11-02 08:53:14 +0900

    add admin combo list

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,13 +1,17 @@
+import { AdminComboProductList } from '@/components/admin/admin-combo-product-list'
 import { AdminProductList } from '@/components/admin/admin-product-list'
+import { ComboProduct } from '@/types/combo-product'
 import { Product } from '@/types/product'
-import { getProductData } from '@/utils/api'
+import { getComboProductData, getProductData } from '@/utils/api'
 
 export default async function Admin() {
   const products = (await getProductData()) as Product[]
+  const comboProducts = (await getComboProductData()) as ComboProduct[]
 
   return (
     <div className="bg-beige-light overflow-x-hidden">
       <AdminProductList data={products} />
+      <AdminComboProductList data={comboProducts} />
     </div>
   )
 }
---
+++ components/admin/admin-combo-product-list.tsx
@@ -0,0 +1,36 @@
+'use client'
+import { useComboProductStore } from '@/store/combo-product-store'
+import { ComboProduct } from '@/types/combo-product'
+import { useEffect } from 'react'
+import { ComboProductCard } from '../combo-product-card'
+import { ProductEditDialog } from './product-edit-dialog'
+
+interface AdminComboProductProps {
+  data: ComboProduct[]
+}
+
+export const AdminComboProductList = ({ data }: AdminComboProductProps) => {
+  const comboProducts = useComboProductStore((state) => state.comboProducts)
+  const setComboProducts = useComboProductStore(
+    (state) => state.setComboProducts,
+  )
+
+  useEffect(() => setComboProducts(data), [data])
+
+  return (
+    <div className="relative before:top-0 before:right-[93%] before:bottom-0 before:absolute flex flex-col gap-16 max-sm:before:hidden before:bg-blue py-24 before:w-[calc(100vw-93%)] container">
+      <h3 className="font-black font-montserrat text-3xl text-blue text-center uppercase">
+        Edit combo cards
+      </h3>
+      <div className="gap-8 grid grid-cols-1 sm:grid-cols-2">
+        {comboProducts &&
+          comboProducts.map((product) => (
+            <div className="relative" key={product.id}>
+              <ComboProductCard product={product} />
+              <ProductEditDialog product={product} />
+            </div>
+          ))}
+      </div>
+    </div>
+  )
+}
